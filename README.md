625fc5334fdd332ebafe383ffdeded9f91d705fa
PJC-1 <jumpzine@gmail.com>
2018-01-25 21:24:14 -0800

    setup middleware and mongodb in app.js

--- README.md
+++ README.md
@@ -13,3 +13,18 @@ Helpful Links
 >
 
 ----------
+
+
+Node.js
+-------------
+>  
+**process.exit()**
+>
+> The ```process.exit()``` method instructs *node.js* to terminate the process synchronously with an exit status of ```code```. If ```code``` is omitted, exit uses either the 'success' code ```0``` or the value of ```process.exitCode``` if it has been set. *Node.js* will not terminate until all the *exit* event listeners are called.
+>
+>  ```process.exit(1)``` to exit with a *failure* code.
+>  
+>  Link to more information on ```process.exit()``` at the [Node.js documentation](https://nodejs.org/api/process.html#process_process_exit_code).
+>  
+
+----------
--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
-
 var port = 4200;
 
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/mernTest')
+  .then(() => {
+    console.log('mongodb connection success!');
+  })
+  .catch(err => {
+    console.log('error connecting to mongodb...', err.stack);
+    process.exit(1);
+  });
+
+var itemRouter = require('./src/routes/itemRouter');
+
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/items', itemRouter);
+
 app.listen(port, function(){
-  console.log("your listening to smooth sounds of port 4200...");
+  console.log(`your listening to smooth sounds of port ${port}...`);
 });
